416ccab3e3047a02c6a076b4914d9753e4a07402
nicksulia <nicksuliava@gmail.com>
2017-12-22 13:34:39 +0200

    Filter Bar added to App for the test purposes

--- src/components/search/filter-bar/FilterBar.js
+++ src/components/search/filter-bar/FilterBar.js
@@ -4,9 +4,17 @@ import Select2 from 'react-select2-wrapper';
 class FilterBar extends PureComponent {
     render() {
         return (
-            <div>
-                Filter Bar
-            </div>
+            <Select2
+                data={[
+                    { text: 'bug', id: 1 },
+                    { text: 'feature', id: 2 },
+                    { text: 'documents', id: 3 },
+                    { text: 'discussion', id: 4 },
+                ]}
+                options={{
+                    placeholder: 'search by tags',
+                }}
+            />
         );
     }
 }
