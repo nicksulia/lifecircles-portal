daaae14eaf13bd2794c867060cb2eab7d1ee9424
nicksulia <nicksuliava@gmail.com>
2017-12-22 12:45:44 +0200

    Default state extended with filters

---
+++ src/reducers/defaultState.js
@@ -0,0 +1,26 @@
+export const filterState = Object.freeze({
+    "age": {
+        "0-1": false,
+        "2-5": false,
+        "6-10": false,
+        "11-16": false,
+        "17-30": false,
+        "30+": false
+    },
+    "diagnosis": {
+        "autism": false,
+        "dts": false
+    },
+    "price_level": {
+        "free": false,
+        "low": false,
+        "medium": false,
+        "high": false
+    },
+    "tags": {
+        "education": false,
+        "children": false,
+        "parents_included": false,
+        "rehabilitation": false
+    }
+});
