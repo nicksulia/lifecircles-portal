884272e926ee8704e656758516a1c27327763827
Nick <nicksuliava@gmail.com>
2017-12-24 17:59:33 +0200

    Added sass loader

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,10 @@ module.exports = {
                 use: ['style-loader', 'css-loader']
             },
             {
+                test: /\.s(a|c)ss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
                 test: /\.svg$/,
                 use: 'svg-url-loader'
             }
