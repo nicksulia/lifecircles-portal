dd5d1b3fea7317dc7cb853632cd1b8815403d914
Nick <nicksuliava@gmail.com>
2017-12-25 01:09:00 +0200

    Default state for filters refactored

--- src/reducers/defaultState.js
+++ src/reducers/defaultState.js
@@ -1,26 +1,37 @@
 export const filterState = Object.freeze({
-    "age": {
-        "0-1": false,
-        "2-5": false,
-        "6-10": false,
-        "11-16": false,
-        "17-30": false,
-        "30+": false
+    "age":{
+        label: "Kid's age",
+        options: [
+            {label: "From 0 to 1", value:"0-1", checked: false},
+            {label: "From 2 to 5", value:"2-5", checked: false},
+            {label: "From 6 to 10", value:"6-10", checked: false},
+            {label: "From 11 to 16", value:"11-16", checked: false},
+            {label: "From 17 to 30", value:"17-30", checked: false},
+            {label: "From 30", value:"30+", checked: false}
+        ]
     },
     "diagnosis": {
-        "autism": false,
-        "dts": false
+        label: "Diagnosis",
+        options:[
+            {label: "Autism", value:"autism", checked: false},
+            {label: "DTS", value:"dts", checked: false},
+        ]
     },
     "price_level": {
-        "free": false,
-        "low": false,
-        "medium": false,
-        "high": false
+        label:"Payment",
+        options:[
+            {label: "Free", value:"free", checked: false},
+            {label: "Low", value:"low", checked: false},
+            {label: "Medium", value:"medium", checked: false},
+            {label: "High", value:"high", checked: false},
+        ]
     },
     "tags": {
-        "education": false,
-        "children": false,
-        "parents_included": false,
-        "rehabilitation": false
+        options:[
+            {label: "Education", value:"education", checked: false},
+            //{label: "With children", value:"children", checked: false},
+            {label: "With parents", value:"parents_included", checked: false},
+            {label: "Rehabilitation", value:"rehabilitation", checked: false},
+        ]
     }
 });
