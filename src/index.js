c08da962838185b971d0909a0ebe1a76823fdda2
Mykola Suliava <mykola.suliava@avid.com>
2017-12-26 15:21:54 +0200

    Store inited in index + filtration

--- src/index.js
+++ src/index.js
@@ -4,8 +4,13 @@ import Application from './components/App.js';
 import store from './store'
 import { Provider } from 'react-redux';
 import {getSpecialistsCall} from './actions/mapActions.js';
+import {filterSpecialists} from './actions/filterActions.js';
 
+const temp = store.getState() || {};
 store.dispatch(getSpecialistsCall());
+store.dispatch(filterSpecialists(temp.mapState.activeFilters, temp.mapState.specialists));
+
+
 
 ReactDOM.render(
     <Provider store={store}>
