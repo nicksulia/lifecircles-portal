968311f125ba7ae046037a0b367883a1cd3561a5
Mykola Suliava <mykola.suliava@avid.com>
2017-12-26 11:41:04 +0200

    reducers refactored

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,8 @@
 import { combineReducers } from 'redux';
+import filterState from './filterStateReducer.js';
+import mapState from './mapReducer.js';
 
 export default combineReducers({
-
+    filterState,
+    mapState
 });
