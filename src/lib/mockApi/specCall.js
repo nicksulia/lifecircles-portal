28af13b64e5e734ea4637ede940de8b00b443ae9
Mykola Suliava <mykola.suliava@avid.com>
2017-12-26 15:21:10 +0200

    lib fixed

--- src/lib/mockApi/specCall.js
+++ src/lib/mockApi/specCall.js
@@ -4,17 +4,14 @@ export const getSpecialists = () => {
     return specialists;
 }
 
-export const filterSpecialists = (filters, specialists) => {
-    debugger;
-    let temp = [...specialists];
+export const filterSpecialists = (filters) => {
+    let temp = [...specialists].map((specialist) => ({...specialist}));
     for (let key in filters) {
         filters[key].forEach((option) => {
             temp = temp.filter( specialist => {
                 return specialist.categories[key].includes(option);
             })
         });
-
     }
-    debugger;
     return temp;
 }
