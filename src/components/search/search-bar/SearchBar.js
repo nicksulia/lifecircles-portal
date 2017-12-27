fe038a735bad83f65ac2f789d98235c5ad92da39
Mykola Suliava <mykola.suliava@avid.com>
2017-12-27 18:58:47 +0200

    Search refactored

--- src/components/search/search-bar/SearchBar.js
+++ src/components/search/search-bar/SearchBar.js
@@ -39,10 +39,16 @@ class SearchBar extends PureComponent {
         return (
             <div className="search-bar-input-wrapper">
                 <input type="text" className="search-bar-input" placeholder="Ex. Summer Camp"/>
-                <div className="search-bar-dropdown-current" onClick={() => { this.setState({
+                <div className="search-bar-input-icon-wrapper">
+                    <div className="search-bar-input-icon"></div>
+                </div>
+                <div className="search-bar-dropdown-current" onClick={() => { if(!this.state.isShowed){this.setState({
                     ...this.state,
                     isShowed: true
-                }) } }>Search {this.state.current}</div>
+                }) } else this.setState({
+                    ...this.state,
+                    isShowed: false
+                }) } }>Search {this.state.current} <div className="search-bar-dropdown-icon"/></div>
                 {this.state.isShowed &&
                     <div className="search-bar-dropdown-list-wrapper">
                         {this.state.dropdownList.map((option) =>
